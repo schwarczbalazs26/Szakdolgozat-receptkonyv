cb5fc5f40a674df7c47ef49e06113682cf62bcab
schwarczbalazs26 <schwarcz.balazs.2019ce@bankitatabanya.hu>
2024-03-08 11:16:31 +0100

    header location fix:D

--- userdata/loginValidation.php
+++ userdata/loginValidation.php
@@ -1,28 +1,26 @@
 <?php
- 
+
 require_once "../helpers/mysql.php";
 $db = new DataBase();
 $nev = $_POST['username_field'];
 $password = $_POST['password_field'];
- 
+
 $sql = "SELECT * FROM `felhasznalok` WHERE `nev` = '$nev'";
- 
+
 $result = $db::$conn->query($sql);
- 
+
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
     if (password_verify($password, $row['jelszo'])) {
         session_start();
         $_SESSION['username_field'] = $username;
-        header("Location: ../homepage/homepage.php?page=arajanlat_keszites"); //popup
+        header("Location: "); //popup
         exit;
     } else {
-        header("Location: index.php?error=a"); //popup kell
+        header("Location: "); //popup kell
         exit;
     }
 } else {
-    header("Location: index.php?error=incorrect_credentials");
+    header("Location: ");
     exit;
 }
- 
-?>
